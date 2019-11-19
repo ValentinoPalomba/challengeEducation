0ae2d10c92db4f61d9315044161be3f00b8d67d4
ciro vitale <c.vitale94@hotmail.com>
2019-11-19 10:53:06 +0100

    settings

--- Education/Settings.swift
+++ Education/Settings.swift
@@ -11,9 +11,29 @@ import UIKit
 class settings : UIViewController {
     
     @IBAction func LinkSettings(_ sender: Any) {
+       
+        let alertController = UIAlertController (title: "Title", message: "Go to Settings?", preferredStyle: .alert)
+
+        let settingsAction = UIAlertAction(title: "Settings", style: .default) { (_) -> Void in
+
+            guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+                return
+            }
+
+            if UIApplication.shared.canOpenURL(settingsUrl) {
+                UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                    print("Settings opened: \(success)") // Prints true
+                })
+            }
+        }
+        alertController.addAction(settingsAction)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        alertController.addAction(cancelAction)
+
+        present(alertController, animated: true, completion: nil)
         
-     
-                        UIApplication.shared.open(URL(string:"App-Prefs:root=General&path=Keyboard")!) 
     }
+
     
 }
+
